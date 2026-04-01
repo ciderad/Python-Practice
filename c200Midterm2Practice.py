88ce375cc6132532a2c93af49a45607b0db3e689
Cidera <125624578+ciderad@users.noreply.github.com>
2026-04-01 11:13:49 -0400

    Revise header and list topics for Midterm 2
    Updated header formatting and added topics for Midterm 2 practice.

--- c200Midterm2Practice.py
+++ c200Midterm2Practice.py
@@ -1,2 +1,31 @@
-## This is practice for Midterm 2, following these topics:
+### This is practice for Midterm 2, following these topics:
+###Cidera Dukuray:
+		
+"""
+•	Dictionary view objects: dict_keys, dict_values, dict_items (immutable)
+	•	Dictionaries: dict (mutable)
+	•	Text file objects: TextIOWrapper (mutable)
+	•	Sets: set (mutable, less important)
 
+
+	•	unbounded loops
+	•	modulus and modulo operator
+	•	eval()
+	•	integer division
+	•	Lambda functions
+ 
+	•	recursion
+	•	base case
+	•	induction
+	•	tail recursion
+	•	memoization
+ 
+	•	persistent storage
+	•	file object
+	•	file
+	•	filename
+	•	mode (for a file object)
+	•	read mode
+	•	write mode
+  """
+if f in g
